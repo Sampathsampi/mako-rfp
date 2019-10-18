2cf426ddc1747b63abecf3078752627a337c7af3
Sampathsampi <52911652+Sampathsampi@users.noreply.github.com>
2019-10-18 20:30:49 +0530

    initial commit
    initial comit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    console.log("we are");
+   $(' .ng-star-inserted').on("hover", function(){
+        
+              $(" .fa-check").css({"color":"red"});
+        });
+         
+      });
+        
